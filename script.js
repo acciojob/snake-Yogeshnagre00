7e4ca40c10eeb8113bef7bcb797dc0e46f4108c2
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-07-03 22:48:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,75 +1,142 @@
 //your code here
-document.addEventListener("DOMContentLoaded", () => {
-  const gameContainer = document.getElementById("gameContainer");
-  const scoreElement = document.getElementById("score");
+const gameContainer = document.getElementById("gameContainer");
+const scoreElement = document.getElementById("score");
 
-  const rows = 10;
-  const cols = 10;
-  const pixelSize = 40;
+const ROW_COUNT = 20;
+const COLUMN_COUNT = 20;
 
-  let snake = [{ row: 20, col: 1 }];
-  let food = generateFood();
-  let direction = "right";
-  let score = 0;
-  let gameLoop;
+let score = 0;
 
-  createGrid();
-
-  function createGrid() {
-    for (let row = 1; row <= rows; row++) {
-      for (let col = 1; col <= cols; col++) {
+// Initialize the grid
+for (let row = 0; row < ROW_COUNT; row++) {
+    for (let column = 0; column < COLUMN_COUNT; column++) {
         const pixel = document.createElement("div");
-        pixel.className = "pixel";
-        pixel.style.top = (row - 1) * pixelSize + "px";
-        pixel.style.left = (col - 1) * pixelSize + "px";
-        pixel.id = "pixel" + (row - 1) * cols + col;
+        pixel.classList.add("pixel");
+        pixel.id = `pixel${row * COLUMN_COUNT + column}`;
         gameContainer.appendChild(pixel);
-      }
     }
-  }
+}
 
-  function generateFood() {
-    const foodPixel = document.createElement("div");
-    foodPixel.className = "pixel food";
-    let randomPixelId;
-    do {
-      randomPixelId = Math.floor(Math.random() * (rows * cols)) + 1;
-    } while (
-      document.getElementById("pixel" + randomPixelId).classList.contains("snakeBodyPixel")
-    );
-    foodPixel.id = "pixel" + randomPixelId;
-    gameContainer.appendChild(foodPixel);
-    return randomPixelId;
-  }
+// Create the snake body
+const snakeBody = [1, 0];
+snakeBody.forEach(index => {
+    const pixel = document.getElementById(`pixel${index}`);
+    pixel.classList.add("snakeBodyPixel");
+});
 
-  function moveSnake() {
-    const head = { ...snake[0] };
+// Create the food
+let foodIndex = generateRandomIndex();
+const foodPixel = document.getElementById(`pixel${foodIndex}`);
+foodPixel.classList.add("food");
 
-    if (direction === "up") head.row--;
-    else if (direction === "down") head.row++;
-    else if (direction === "left") head.col--;
-    else if (direction === "right") head.col++;
+// Set up the game loop
+let direction = "right";
+let intervalId = setInterval(moveSnake, 100);
 
-    if (
-      head.row < 1 ||
-      head.row > rows ||
-      head.col < 1 ||
-      head.col > cols ||
-      document.getElementById("pixel" + (head.row - 1) * cols + head.col).classList.contains(
-        "snakeBodyPixel"
-      )
-    ) {
-      gameOver();
-      return;
+function moveSnake() {
+    const headIndex = snakeBody[0];
+    let nextIndex;
+
+    if (direction === "right") {
+        if ((headIndex + 1) % COLUMN_COUNT === 0) {
+            // Snake has hit the right wall
+            clearInterval(intervalId);
+            alert(`Game Over! Your score is ${score}.`);
+            return;
+        }
+        nextIndex = headIndex + 1;
+    } else if (direction === "left") {
+        if (headIndex % COLUMN_COUNT === 0) {
+            // Snake has hit the left wall
+            clearInterval(intervalId);
+            alert(`Game Over! Your score is ${score}.`);
+            return;
+        }
+        nextIndex = headIndex - 1;
+    } else if (direction === "up") {
+        if (headIndex < COLUMN_COUNT) {
+            // Snake has hit the top wall
+            clearInterval(intervalId);
+            alert(`Game Over! Your score is ${score}.`);
+            return;
+        }
+        nextIndex = headIndex - COLUMN_COUNT;
+    } else if (direction === "down") {
+        if (headIndex >= (ROW_COUNT - 1) * COLUMN_COUNT) {
+            // Snake has hit the bottom wall
+            clearInterval(intervalId);
+            alert(`Game Over! Your score is ${score}.`);
+            return;
+        }
+        nextIndex = headIndex
+//your code here
+const gameContainer = document.getElementById("gameContainer");
+const scoreElement = document.getElementById("score");
+
+const ROW_COUNT = 20;
+const COLUMN_COUNT = 20;
+
+let score = 0;
+
+// Initialize the grid
+for (let row = 0; row < ROW_COUNT; row++) {
+    for (let column = 0; column < COLUMN_COUNT; column++) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        pixel.id = `pixel${row * COLUMN_COUNT + column}`;
+        gameContainer.appendChild(pixel);
     }
+}
 
-    snake.unshift(head);
+// Create the snake body
+const snakeBody = [1, 0];
+snakeBody.forEach(index => {
+    const pixel = document.getElementById(`pixel${index}`);
+    pixel.classList.add("snakeBodyPixel");
+});
 
-    if (head.row === Math.floor(food / cols) + 1 && head.col === (food - 1) % cols + 1) {
-      score++;
-      scoreElement.textContent = score;
-      document.getElementById("pixel" + food).remove();
-      food = generateFood();
-    } else {
-      const tail = snake.pop();
-      document.getElementById("pixel" + (tail.row -
+// Create the food
+let foodIndex = generateRandomIndex();
+const foodPixel = document.getElementById(`pixel${foodIndex}`);
+foodPixel.classList.add("food");
+
+// Set up the game loop
+let direction = "right";
+let intervalId = setInterval(moveSnake, 100);
+
+function moveSnake() {
+    const headIndex = snakeBody[0];
+    let nextIndex;
+
+    if (direction === "right") {
+        if ((headIndex + 1) % COLUMN_COUNT === 0) {
+            // Snake has hit the right wall
+            clearInterval(intervalId);
+            alert(`Game Over! Your score is ${score}.`);
+            return;
+        }
+        nextIndex = headIndex + 1;
+    } else if (direction === "left") {
+        if (headIndex % COLUMN_COUNT === 0) {
+            // Snake has hit the left wall
+            clearInterval(intervalId);
+            alert(`Game Over! Your score is ${score}.`);
+            return;
+        }
+        nextIndex = headIndex - 1;
+    } else if (direction === "up") {
+        if (headIndex < COLUMN_COUNT) {
+            // Snake has hit the top wall
+            clearInterval(intervalId);
+            alert(`Game Over! Your score is ${score}.`);
+            return;
+        }
+        nextIndex = headIndex - COLUMN_COUNT;
+    } else if (direction === "down") {
+        if (headIndex >= (ROW_COUNT - 1) * COLUMN_COUNT) {
+            // Snake has hit the bottom wall
+            clearInterval(intervalId);
+            alert(`Game Over! Your score is ${score}.`);
+            return;
+        }
+        nextIndex = headIndex
